9625f0698d90ead5a33532a1c7bfb6eb6ba43e82
Valluri Satyaprakash <prakash4455@gmail.com>
2021-09-09 17:28:45 +0530

    Adding slot content programatically

--- webcomponents templates and slots/script.js
+++ webcomponents templates and slots/script.js
@@ -5,8 +5,16 @@ class MyComponent extends HTMLElement {
     const templateElement = document.getElementById("id-template");
     const templateContent = templateElement.content;
 
+    //<span slot='name-slot'>new Text</span>
+    const spanText = document.createElement("span");
+    spanText.setAttribute("slot", "name-slot");
+    const inputText = document.createElement("span");
+    inputText.textContent = this.getAttribute("fieldValue");
+    spanText.appendChild(inputText);
+
     const parentNode = templateContent.cloneNode(true);
     this.shadowRoot.appendChild(parentNode);
+    this.appendChild(spanText);
   }
   connectedCallback() {}
 }
